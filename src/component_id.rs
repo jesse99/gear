68c51bf9fd52a13ca07c3ecbc0778636b5e7618e
Jesse Vorisek <jesse.vorisek@gmail.com>
2024-02-26 15:34:59 -0800

    Added Display trait to ComponentId

--- src/component_id.rs
+++ src/component_id.rs
@@ -1,4 +1,5 @@
 use core::sync::atomic::AtomicU32;
+use std::fmt::{self, Formatter};
 use std::sync::atomic::Ordering;
 
 /// Used to identify components.
@@ -9,4 +10,10 @@ pub static NEXT_COMPONENT_ID: AtomicU32 = AtomicU32::new(1);
 
 pub fn next_component_id() -> ComponentId {
     ComponentId(NEXT_COMPONENT_ID.fetch_add(1, Ordering::Relaxed))
+}
+
+impl fmt::Display for ComponentId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "#{}", self.0)
+    }
 }
